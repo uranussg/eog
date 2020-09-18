5f8a687d2243c72f2e57abf02d08ff4623f9e309
uranussg <uranussg@gmail.com>
2020-09-18 16:05:41 -0400

    input ele

---
+++ src/components/Input.tsx
@@ -0,0 +1,112 @@
+import React from 'react';
+import clsx from 'clsx';
+import { createStyles, makeStyles, useTheme, Theme } from '@material-ui/core/styles';
+import Input from '@material-ui/core/Input';
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
+import FormControl from '@material-ui/core/FormControl';
+import ListItemText from '@material-ui/core/ListItemText';
+import Select from '@material-ui/core/Select';
+import Checkbox from '@material-ui/core/Checkbox';
+import Chip from '@material-ui/core/Chip';
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    formControl: {
+      margin: theme.spacing(1),
+      minWidth: 120,
+      maxWidth: 300,
+    },
+    chips: {
+      display: 'flex',
+      flexWrap: 'wrap',
+    },
+    chip: {
+      margin: 2,
+    },
+    noLabel: {
+      marginTop: theme.spacing(3),
+    },
+  }),
+);
+
+const ITEM_HEIGHT = 48;
+const ITEM_PADDING_TOP = 8;
+const MenuProps = {
+  PaperProps: {
+    style: {
+      maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
+      width: 250,
+    },
+  },
+};
+
+const options = [
+  'flareTemp',
+  'casingPressure',
+  'injValve',
+  'oilTemp',
+  'tubingPressure',
+  'waterTemp',
+];
+
+function getStyles(option: string, displayOption: string[], theme: Theme) {
+  return {
+    fontWeight:
+      displayOption.indexOf(option) === -1
+        ? theme.typography.fontWeightRegular
+        : theme.typography.fontWeightMedium,
+  };
+}
+
+export default function MultipleSelect() {
+  const classes = useStyles();
+  const theme = useTheme();
+  const [displayOption, setdisplayOption] = React.useState<string[]>([]);
+
+  const handleChange = (event: React.ChangeEvent<{ value: unknown }>) => {
+    setdisplayOption(event.target.value as string[]);
+  };
+
+  const handleChangeMultiple = (event: React.ChangeEvent<{ value: unknown }>) => {
+    const { options } = event.target as HTMLSelectElement;
+    const value: string[] = [];
+    for (let i = 0, l = options.length; i < l; i += 1) {
+      if (options[i].selected) {
+        value.push(options[i].value);
+      }
+    }
+    setdisplayOption(value);
+  };
+
+  return (
+    <div>
+      <FormControl className={classes.formControl}>
+        <InputLabel id="input-options">Chip</InputLabel>
+        <Select
+          labelId="input-options"
+          id="options"
+          multiple
+          value={displayOption}
+          onChange={handleChange}
+          input={<Input id="select-options" />}
+          renderValue={(selected) => (
+            <div className={classes.chips}>
+              {(selected as string[]).map((value) => (
+                <Chip key={value} label={value} className={classes.chip} />
+              ))}
+            </div>
+          )}
+          MenuProps={MenuProps}
+        >
+          {options.map((option) => (
+            <MenuItem key={option} value={option} style={getStyles(option, displayOption, theme)}>
+              {option}
+            </MenuItem>
+          ))}
+        </Select>
+      </FormControl>
+      
+    </div>
+  );
+}
