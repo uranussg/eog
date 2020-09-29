5e4d715df8c64c40a9fec8afd39418253914f0c2
uranussg <uranussg@gmail.com>
2020-09-29 14:29:24 -0400

    chart

--- src/Features/Chart/Chart.tsx
+++ src/Features/Chart/Chart.tsx
@@ -7,7 +7,9 @@ import LinearProgress from '@material-ui/core/LinearProgress';
 import Chip from '../../components/Chip';
 import { IState } from '../../store';
 import { useStore } from 'react-redux'
-
+import {
+  LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend,
+} from 'recharts';
 const client = createClient({
   url: 'https://react.eogresources.com/graphql',
 });
@@ -55,10 +57,11 @@ const Data = () => {
   const query = `
   query($heartBeat: Timestamp){
     getMultipleMeasurements(${input}){
+      metric,
       measurements{
         metric
         time: at,
-        value:value,
+        value,
     }
     }
   }
@@ -71,7 +74,9 @@ const Data = () => {
     }
   })
   const { fetching, data, error } = result;
-  
+  data.foreach((dataom: { measurements: { [x: string]: any; value: any; }; metric: string | number; }) => {
+    dataom.measurements[dataom.metric] = dataom.measurements.value
+  })
     useEffect(() => {
     if (error) {
       dispatch(actions.dataErrorReceived({ error: error.message }));
@@ -84,6 +89,30 @@ const Data = () => {
 
   // if (fetching) return <LinearProgress />;
   // return <Chip />;
+  const axis = labels.map((label: any) => {
+    return <YAxis yAxisId={`${label}`} />
+  })
+  const colors = ["#8884d8","#82ca9d","#ffc658", "green", "red", "blue"]
+  const  lines = labels.map((label: any, idx: number) => {
+    return <Line yAxisId={`${label}`} type="monotone" dataKey={`${label}`} stroke={`${colors[idx]}`} activeDot={{ r: 8 }} />
+  })
+  return (
+    <LineChart
+      width={500}
+      height={300}
+      data={data.getMultipleMeasurements}
+      margin={{
+        top: 5, right: 30, left: 20, bottom: 5,
+      }}
+    >
+      <CartesianGrid strokeDasharray="3 3" />
+      <XAxis dataKey="time" />
+      {axis}
+      <Tooltip />
+      <Legend />
+      {lines}
+    </LineChart>
+  );
 };
 
 
